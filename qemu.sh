736c8bf4aac5196f5cc2079a8bc4d9b701656ea3
Darkone <gponcon@gmail.com>
2024-02-18 14:44:18 -0300

    Added splice

--- qemu.sh
+++ qemu.sh
@@ -72,7 +72,7 @@ qemu-system-x86_64 $IMG \
 	-accel kvm \
 	-cpu host \
 	-smp cores=2,threads=2,sockets=1,maxcpus=4 \
-	-display gtk \
+	-display gtk -spice port=5900 \
 	-device virtio-vga,edid=on,xres=1920,yres=1080 \
 	-full-screen \
 	-device virtio-net,netdev=net0 \
